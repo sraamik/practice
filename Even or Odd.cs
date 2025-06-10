56188091fe3dd3b660a7d7b41237c5c2a09d6900
Vijay kumar devi <30660278+sraamik@users.noreply.github.com>
2025-06-10 15:54:50 +0200

    Update Even or Odd.cs

--- Even or Odd.cs
+++ Even or Odd.cs
@@ -17,4 +17,39 @@ namespace Coding.Excercise
 
 
     }
+
+    public class Program 
+    {
+        public static void Main(string[] args)
+
+        {
+            NumberChecker checker = new NumberChecker();
+
+            Console.WriteLine("Let's check if a number is even or odd!");
+            Console.WriteLine("Please enter an integer: ");
+
+            string input = Console.ReadLine();
+
+            int number;
+
+            if (checker.IsEven(number))
+            {
+                Console.WriteLine($"\n Result: {} is an EVEN number.");
+            }
+            else
+            {
+                Console.WriteLine($"\n Result: {} is an ODD number.");
+            }
+
+
+        }
+        else
+        {
+            Console.WriteLine("\n Error: That was not a valid integer. please enter valid integer.");
+
+        }
+
+        Console.WriteLine("\n Press any key to exit. ");
+
+    }
 }
