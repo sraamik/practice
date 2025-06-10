6ee9bd5eae1a5a8bb78261e826e103a009100a47
vijay <vkdevi@softstrategy.it>
2025-06-10 15:10:23 +0200

    rectangle

---
+++ rectangle.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Coding.Excercise
+{
+    public class Rectangle 
+    {
+        public double CalculatePerimeter(double lenght, double width)
+        {
+            return 2  * (length+width);
+        }
+    }
+}
