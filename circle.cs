e7b08bc13dba70de468da522cbda5d558c85a311
vijay <vkdevi@softstrategy.it>
2025-06-10 15:02:52 +0200

    circle

---
+++ circle.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Coding.Excercise
+{
+    public class circle
+    {
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius
+
+        }
+
+    }
+    
+}
