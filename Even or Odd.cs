755047c5108de3c545a15d4ef95e945145e2d2cf
vijay <vkdevi@softstrategy.it>
2025-06-10 15:34:57 +0200

    Even or Odd

---
+++ Even or Odd.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Coding.Excercise
+{
+    public class NumberChecker
+    {
+        public bool IsEven(int number)
+        {
+            return number %2 == 0;
+        }
+
+
+        public bool IsOdd(int number) 
+        {
+            return number %2 != 0;
+        }
+
+
+    }
+}
